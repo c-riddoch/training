e1918876a9fd26f4d93ca31ccd9c9646c1d4cea8
Cath Riddoch <catherine.riddoch@gov.scot>
2025-06-06 14:31:23 +0100

    adds birthday function

---
+++ R/count_days_cath.R
@@ -0,0 +1,22 @@
+#' count_days_cath
+#'
+#' @description this is a training function which counts the number of days
+#' until Cath's birthday
+#' @return integer
+#' @export
+
+count_days_cath<- function(){
+
+  # set birthday
+  birthday <- as.Date("2025-08-15")
+
+  # Get today's date
+  today_date <- Sys.Date()
+
+  # Calculate the number of days
+  days_to_birthday <- as.numeric(difftime(birthday, today_date, units = "days"))
+
+  print(days_to_birthday)
+}
+
+
